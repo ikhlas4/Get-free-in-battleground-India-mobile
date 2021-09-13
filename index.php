a28e9528a627db3b74c3dbe754b9f14f3c76dd5d
ikhlas sayyed <86350527+ikhlas4@users.noreply.github.com>
2021-09-13 17:24:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <title>HTML</title>
+  
+  <!-- HTML -->
+  
+
+  <!-- Custom Styles -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="header" id="nav">
+    <h1 id="point">IS point</h1>
+  </div>
+  
+ 
+  
+  
+  <h1 id="h"><b>Post by ikhlas </b></h1>
+  
+  <div id="post">
+    <br>
+    <h3>hello I'm ikhlas sayyed</h3>
+    <p>I am sharing you Coding in your mobile phone if You do not have
+    computer and laptop you coding in mobile.....<a href="post/post1.html">learn more</a></p>
+  </div>
+  <br>
+  
+  
+   <div id="post">
+     <br>
+    <h3>about termux</h3> 
+    <p>Termux is a terminal emulator in Android that  for running a Linux environment on an android device. In addition, various Linux software can be installed through the application's package manager....<a href="post/post2.html">learn more</a></p>
+   </div>
+    
+
+  
+  
+  <!--footer
+  <div class="footer">
+    
+    
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-instagram"></a>
+    
+   
+  </div>
+  -->
+  <!-- Project -->
+  <script src="main.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+</body>
+</html>
